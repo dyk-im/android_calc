14561add314cdc85d0b8bf0c94c8603415810a5c
dyk-im <djskaldrjfj@naver.com>
2024-03-31 20:52:00 +0900

    깃 ã저í터미널 셋팅

--- app/src/main/java/com/example/practice3/MainActivity.java
+++ app/src/main/java/com/example/practice3/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+// okok
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
