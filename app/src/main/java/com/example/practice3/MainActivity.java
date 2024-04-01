750b007e54221fe819d7a610ebd8f089c0291a29
dyk-im <djskaldrjfj@naver.com>
2024-04-01 21:52:26 +0900

    calc

--- app/src/main/java/com/example/practice3/MainActivity.java
+++ app/src/main/java/com/example/practice3/MainActivity.java
@@ -3,12 +3,71 @@ package com.example.practice3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.MotionEvent;
+import android.widget.*;
+
+import org.w3c.dom.Text;
+
 
 public class MainActivity extends AppCompatActivity {
-// okok
+    EditText edit1, edit2;
+    Button btnAdd, btnSub, btnMul, btnDiv;
+    TextView textResult;
+    String num1, num2;
+    Double result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("202035117 김동윤");
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        edit1 = (EditText)findViewById(R.id.Edit1);
+        edit2 = (EditText)findViewById(R.id.Edit2);
+
+        btnAdd = (Button)findViewById(R.id.BtnAdd);
+        btnSub = (Button)findViewById(R.id.BtnSub);
+        btnMul = (Button)findViewById(R.id.BtnMul);
+        btnDiv = (Button)findViewById(R.id.BtnDiv);
+
+        textResult = (TextView)findViewById(R.id.TextResult);
+
+        btnAdd.setOnTouchListener(new View.OnTouchListener(){
+            public boolean onTouch(View arg0, MotionEvent arg1) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                result = Double.parseDouble(num1) + Double.parseDouble(num2);
+                textResult.setText("계산 결과 : " + result.toString());
+                return false;
+            }
+        });
+        btnSub.setOnTouchListener(new View.OnTouchListener(){
+            public boolean onTouch(View arg0, MotionEvent arg1) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                result = Double.parseDouble(num1) - Double.parseDouble(num2);
+                textResult.setText("계산 결과 : " + result.toString());
+                return false;
+            }
+        });
+        btnMul.setOnTouchListener(new View.OnTouchListener(){
+            public boolean onTouch(View arg0, MotionEvent arg1) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                result = Double.parseDouble(num1) * Double.parseDouble(num2);
+                textResult.setText("계산 결과 : " + result.toString());
+                return false;
+            }
+        });
+        btnDiv.setOnTouchListener(new View.OnTouchListener(){
+            public boolean onTouch(View arg0, MotionEvent arg1) {
+                num1 = edit1.getText().toString();
+                num2 = edit2.getText().toString();
+                result = Double.parseDouble(num1) / Double.parseDouble(num2);
+                textResult.setText("계산 결과 : " + result.toString());
+                return false;
+            }
+        });
     }
 }
